88321bd7f6732cbf84be31f65db23b3b8ff1e63e
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-04-19 19:08:43 +0300

    Use linenoise.

--- src/main.ml
+++ src/main.ml
@@ -1,2 +1,14 @@
-let () = print_endline "R1ML"
+let prompt = "R1ML> "
+
+let rec repl () =
+    match LNoise.linenoise prompt with
+    | None -> ()
+    | Some input ->
+        let _ = LNoise.history_add input in
+        print_endline input;
+        repl ()
+
+let () =
+    print_endline "R1ML prototype REPL. Press Ctrl+D (on *nix, Ctrl+Z on Windows) to quit.";
+    repl ()
 
