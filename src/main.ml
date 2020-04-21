51850bc2b87d1342af349bd186f7f0a698c2363b
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-04-21 16:12:00 +0300

    REP fixes.

--- src/main.ml
+++ src/main.ml
@@ -6,14 +6,13 @@ let prompt = "R1ML> "
 let ep env stmts =
     let step stmt =
         let {term; typ; eff} : FcTerm.stmt Typer.typing = Typer.check_interaction env stmt in
-        let doc = PPrint.group (FcTerm.stmt_to_doc term
-            ^/^ PPrint.colon ^/^ FcType.to_doc typ
+        PPrint.ToChannel.pretty 1.0 80 stdout (FcTerm.stmt_to_doc term ^^ PPrint.hardline);
+        let doc = PPrint.group (PPrint.colon ^/^ FcType.to_doc typ
             ^/^ PPrint.bang ^/^ Ast.effect_to_doc eff) in
         PPrint.ToChannel.pretty 1.0 80 stdout doc in
     List.iter step stmts
 
-let rec repl () =
-    let env = Typer.Env.interactive () in
+let rec repl env =
     match LNoise.linenoise prompt with
     | None -> ()
     | Some input ->
@@ -28,10 +27,10 @@ let rec repl () =
         with
             | SedlexMenhir.ParseError err ->
                 prerr_endline (SedlexMenhir.string_of_ParseError err));
-        repl ()
+        repl env
 
 let () =
     Hashtbl.randomize ();
     print_endline "R1ML prototype REPL. Press Ctrl+D (on *nix, Ctrl+Z on Windows) to quit.";
-    repl ()
+    repl (Typer.Env.interactive ())
 
